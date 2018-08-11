32d552ff1e2cf5c6264c2c09cecbdb58bf61ac9b
voidpointercast <patrick.daniel.gress@gmail.com>
2018-08-11 12:34:32 -0400

    Restructured mutator to be used on individuals, now.

--- pyvolution/mutation/__init__.py
+++ pyvolution/mutation/__init__.py
@@ -1,11 +1,33 @@
 from typing import Callable
-from pyvolution.types.gene import BaseType, Chromosome
+from pyvolution.types.gene import BaseType
+from pyvolution.types.individual import Individual
 
 Mutator = Callable[[BaseType], BaseType]
 
 
-def mutate(mutator: Mutator, chromosome: Chromosome) -> Chromosome:
-    return type(chromosome)(
-        (i, mutator(base)) for (i, base) in chromosome.items()
+def mutate(mutator: Mutator, individual: Individual) -> Individual:
+    """
+    :param mutator:
+    :param individual:
+    :return:
+    >>> member = Individual({0: [{0: 1, 1: 2}], 1: ({0: 3, 1:4},)}, 0, 'John doe')
+    >>> mutate(lambda x: x**2, member)
+    Individual(karyogram={0: [{0: 1, 1: 4}], 1: ({0: 9, 1: 16},)}, generation=0, name='John doe')
+    """
+
+    mutated = (
+        (
+            position,
+            type(chromosomes)(
+                type(chromosome)(
+                    (p, mutator(base)) for (p, base) in chromosome.items()
+                )
+                for chromosome in chromosomes
+            )
+        )
+        for (position, chromosomes) in individual.karyogram.items()
     )
+    mutant = Individual(**vars(individual))
+    mutant.karyogram = type(individual.karyogram)(mutated)
+    return mutant
 
