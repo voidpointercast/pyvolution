5d21f097ae42ee809e4943838663d2461165916b
voidpointercast <patrick.daniel.gress@gmail.com>
2018-08-11 10:47:19 -0400

    Added doctest

--- pyvolution/evolution/__init__.py
+++ pyvolution/evolution/__init__.py
@@ -22,8 +22,37 @@ def build_evolution_model(
     :param survival:
     :param fitness:
     :return:
+    >>> from string import ascii_letters
+    >>> from random import choice
     >>> from pyvolution.birth import top_individuals_breed
-    >>>
+    >>> from pyvolution.fitness import create_fitness
+    >>> from pyvolution.types.gene import create_linear_mapping, create_chromosome_builder
+    >>> from pyvolution.types.individual import create_individual_builder, create_sequential_naming
+    >>> def fitness(value: str) -> float:
+    ...     return sum(1 for x in value if x.isupper()) + len(set('HELLO WORLD').intersection(set(value)))
+    >>> fitness('HEllo World')
+    7
+    >>> def randdata(size: int, amount: int):
+    ...     return [''.join(choice(ascii_letters) for _ in range(size))  for _ in range(amount)]
+    >>> mapping, remapping = create_linear_mapping(4)
+    >>> ifitness = create_fitness(fitness, remapping, lambda x: x, lambda x: x[0])
+    >>> evolve = build_evolution_model(ifitness, top_individuals_breed(ifitness))
+    >>> cbuilder = create_chromosome_builder(lambda x: [x], mapping, lambda x: x)
+    >>> builder = create_individual_builder(cbuilder, create_sequential_naming(lambda x: x-10))
+    >>> start_pop = [
+    ...     builder(randdata(10, 2), 0) for _ in range(10)
+    ... ]
+    >>> names =  tuple(i.name for i in start_pop)
+    >>> min(names), max(names)
+    (-10, -1)
+    >>> next_gen = tuple(evolve(start_pop, 1))
+    >>> len(next_gen)
+    10
+    >>> names = [i.name for (i, v) in next_gen]
+    >>> len([n for n in names if n < 0])
+    5
+    >>> len([n for n in names if n >= 0])
+    5
     """
 
     def evolve(population: Iterable[Individual], generation: int) -> Iterable[RankedPopulation]:
